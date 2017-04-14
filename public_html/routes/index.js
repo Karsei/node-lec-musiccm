0a776eba4e99532c79a9a291587a2e2cad394761
Karsei <karseidev@gmail.com>
2017-04-14 17:13:14 +0900

    1. Bootstrap 및 Custom CSS 준비 2. 기본 메뉴 구성 3. 라우팅 구성

--- public_html/routes/index.js
+++ public_html/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*********************************************
+ * DEFAULT VARIABLES
+**********************************************/
+var HOMEPAGE_TITLE = "DASHBOARD";
+
+/* GET - Home */
 router.get('/', function(req, res, next) {
-     res.render('index', { title: 'Express' });
+     res.render('index', { title: HOMEPAGE_TITLE });
+});
+router.get('/index', function(req, res, next) {
+     res.render('index', { title: HOMEPAGE_TITLE });
+});
+/* GET - About */
+router.get('/about', function(req, res, next) {
+     res.render('about', { title: HOMEPAGE_TITLE });
+});
+/* GET - Play */
+router.get('/play', function(req, res, next) {
+     res.render('play', { title: HOMEPAGE_TITLE });
+});
+/* GET - Mentor */
+router.get('/mentor', function(req, res, next) {
+     res.render('mentor', { title: HOMEPAGE_TITLE });
+});
+/* GET - Sheet */
+router.get('/sheet', function(req, res, next) {
+     res.render('sheet', { title: HOMEPAGE_TITLE });
 });
 
 module.exports = router;
